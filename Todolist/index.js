1ced50d3f6f87b1ceabcc8d7be8eb2e1c37b357d
nickld40 <34141610+nickld40@users.noreply.github.com>
2018-04-30 01:24:50 +0200

    Get on api/lists still doesnt work

--- Todolist/index.js
+++ Todolist/index.js
@@ -14,9 +14,21 @@ app.use(express.static('public'));
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
+//initialize db
+const sqlite3 = require('sqlite3').verbose();
+
+let db = new sqlite3.Database('data.db', (err)=>{
+	if (err){
+		console.error(err.mesaage);
+	}
+	console.log('connected to data.db database');
+});
+
 app.listen(3000, function(){
 	console.log('listening on port 3000');
 });
+
+//above are initializations only
 
 app.get('/', function(req, res){
 	res.render('app');
@@ -31,51 +43,38 @@ app.get('/api/lists', function(req, res){
 });
 
 
+app.post('/api/lists', function (req,res,incomingList){
+	//variable could need to be parsed by JSON.parse(var) when softcoded
+	 incomingList ={
+		"id": 42,
+  "name": "Die Ultimative ToDo-Liste von Edi",
+  "owner": "xxxfrunobulaxedixxx",
+  "done": 0
+	};
 
-//initialize db
-const sqlite3 = require('sqlite3').verbose();
-
-let db = new sqlite3.Database('data.db', (err)=>{
-	if (err){
-		console.error(err.mesaage);
-	}
-	console.log('connected to data.db database');
+	let space = `INSERT INTO lists(name, done, owner) VALUES (?, ?, ?)`;
+	db.run(space, [incomingList.name, incomingList.done, incomingList.owner], function(err){
+		console.log(err);
+	});
+	res.send();
 });
 
 
-
-
 //FOLLOWING FUNCTION READS THE DATA.DB TABLE CALLED lists
-// IT GETS ALL ROW-ELEMENTS BUT RETURNING THEM DOESNT WORK YES 
+//IT GETS ALL ROW-ELEMENTS BUT RETURNING THEM DOESNT WORK YES
 
-// function sqltoarray()
-// {
-// 	let id = 0;
-// 	let name = ' ';
-// 	let done = 0;
-// 	let owner = ' ';
-//
-// 	var toStringify = [];
-//
-// 	db.each(`SELECT * FROM lists`,  (err, row) => {
-// 		if (err) {
-// 			console.error(err.message);
-// 		}
-// 		// row ist ein Objekt mit den Feldern als Properties
-//
-// 	 	id = row.id;
-// 	 	name = row.name;
-// 	 	done = row.done;
-// 	 	owner = row.owner;
-//
-// 		toStringify = ["pushed"];
-// 		console.log(toStringify);
-//
-// 	//je obj ein neuer {} im JSON string
-// 	//toStringify = toStringify.push();
-//
-// });
-//
-// 		return(JSON.stringify(toStringify));
-//
-// };
+function sqltoarray(){
+	let insert = "[]";
+	db.each(`SELECT * FROM lists`,  (err, row) => {
+		if (err) {
+			console.error(err.message);
+		}
+		// row ist ein Objekt mit den Feldern als Properties
+
+	 let id = row.id;
+	 let name = row.name;
+	 let done = row.done;
+	 let owner = row.owner;
+	});
+	return insert;
+};
