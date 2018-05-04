08816800169bb4df75e332493732bc2d9b631317
Omnickron <njschestag@gmail.com>
2018-05-05 00:16:40 +0200

    added items.js

--- Todolist/Server/Api/_api.js
+++ Todolist/Server/Api/_api.js
@@ -1,3 +1,4 @@
 module.exports = function (app, db) {
   require('./lists.js')(app, db);
+  require('./items.js')(app,db);
 }
---
+++ Todolist/Server/Api/items.js
@@ -0,0 +1,9 @@
+module.exports = function(app,db){
+
+  //SHOW ALL items
+  app.get('/api/items/',function(req,res){
+    console.log("TESTLOG");
+  })
+
+
+}
