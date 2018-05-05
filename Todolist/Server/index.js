0e8287e4d46cc6a67d30a77bcc297a493f576b78
Tom Schneider <tom@sutomaji.net>
2018-05-05 12:32:42 +0200

    express now serves the polymer front-end

--- Todolist/Server/index.js
+++ Todolist/Server/index.js
@@ -1,15 +1,23 @@
+// CONFIG
+const DEVELOPMENT = true;
+// AUTO-GENERATED CONFIG
+const STATIC_FOLDER = DEVELOPMENT ? __dirname + '/../WebClient/' : __dirname + '/../WebClient/build/es5-bundled/';
+const INDEX_HTML = DEVELOPMENT ? __dirname + '/../WebClient/index.html' : __dirname + '/../WebClient/build/es5-bundled/index.html';
+// END CONFIG
+
 const express = require('express');
-const app = express();
-
-// Directly serve all files in public/
-app.use(express.static(__dirname + '/../WebClient/build/es5-bundled/'));
-
+const path = require('path');
 const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-
-//initialize db
 const sqlite3 = require('sqlite3').verbose();
 
+const app = express();
+
+
+// Directly serve all files in public/
+app.use(express.static(path.resolve(STATIC_FOLDER)));
+app.use(bodyParser.json());
+
+// initialize db
 const db = new sqlite3.Database(__dirname + '/Database/data.db', (err)=>{
 	if (err){
 		console.error(err.mesaage);
@@ -18,13 +26,12 @@ const db = new sqlite3.Database(__dirname + '/Database/data.db', (err)=>{
 });
 
 app.listen(3000, function(){
+	if(DEVELOPMENT) console.warn('⚠ running server in development mode. do not use this for production.');
 	console.log('listening on port 3000');
 });
 
-//above are initializations only
-
-app.use('*', function(req, res){
-	res.send(__dirname + '/../WebClient/build/es5-bundled/index.html');
+app.get('*', function(req, res){
+	res.sendFile(path.resolve(INDEX_HTML));
 });
 
 // import api
