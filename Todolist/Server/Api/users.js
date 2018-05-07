d644ebf11cb65a70c4d4fbb0d0bfa28d690dfcd2
KiVi-Kopf <vincent.martin@gmx.net>
2018-05-07 22:48:20 +0200

    "delete" for api/users

--- Todolist/Server/Api/users.js
+++ Todolist/Server/Api/users.js
@@ -1,44 +1,58 @@
 module.exports = function(app,db){
 
-    app.post('/api/users', function(req, res) {
-      let space = `INSERT INTO users(name, password, email) VALUES (?, ?,?)`;
-      db.run(space,[req.body.name,req.body.password,req.body.email], function(err){
-        if (err) {console.log(err)}
-
-res.send([]);
-
-      });
+  //Show all users
+  app.get('/api/users',function(req, res){
+    db.all(`SELECT * FROM users`, function(err, rows){
+      if (err) {
+        console.log(err)
+      }
+        res.send(rows);
     });
-
-
-//show all users
-
-app.get('/api/users',function(req, res){
-  db.all(`SELECT * FROM users`, function(err, rows){
-    if (err) {console.log(err);}
-
-    res.send(rows);
   });
 
-});
 
-//get user by ID
-app.get('/api/users/:id', function(req, res) {
-  id = req.params.id;
-  space = "SELECT * FROM users WHERE id =" + (id.toString());
-  db.all(space, function(err, rows) {
-    if (err) {
-      console.log(err)
-    };
-    res.send(rows);
+  //Show specific user by specifying ID
+  app.get('/api/users/:id', function(req, res){
+    let id = req.params.id;
+    space = `SELECT * FROM users WHERE id =` + (id.toString());
+    db.all(space, function(err, rows) {
+      if (err) {
+        console.log(err)
+      }
+        res.send(rows);
+    });
+  });
 
-  })
-});
+//Add new user
+  app.post('/api/users', function(req, res){
+    let space = `INSERT INTO users(name, password, email) VALUES (?, ?, ?)`;
+    db.run(space,[req.body.name,req.body.password,req.body.email], function(err){
+      if (err) {
+        console.log(err)
+      }
+        res.send([]);
+    });
+  });
 
 
 
+//Delete user by ID
+  app.delete('/api/users/:id',function(req,res){
+    let id = req.params.id;
+    let space = `DELETE FROM users WHERE id =` + (id.toString());
+    db.run(space, function(err){
+      if (err) {
+        console.log(err)
+      };
+    });
+    res.send({})
+  });
 
+};
 
-
-
-}
+//Fragen:
+//1.Wann Semikolon
+//2.Wann let/war
+//3.Wann `` oder "" oder ''
+//4.Wann CAPS
+//5.Brauchen wir noch einen table f
