6c5e3d7d94ec4f1c567aed4751ab996817be1a3a
Tom Schneider <tom@sutomaji.net>
2018-06-05 23:34:34 +0200

    collaborators can only be added once to the list. close #30

--- Todolist/Server/Api/collaborators.js
+++ Todolist/Server/Api/collaborators.js
@@ -4,16 +4,24 @@ module.exports = function(app,db) {
   app.post('/api/collaborators', function(req, res) {
 
     // @todo: check if user is allowed to add collaborator to list
-
     db.all(`select id from users where name = '${req.body.user}' or email = '${req.body.user}'`, function(err, rows) {
 
       if(rows !== undefined && rows.length == 1) {
-        db.run('insert into collaborators (userid, listid, owner) values (?, ?, 0)',
-              [rows[0].id, req.body.listid],
-              function(err){
-                if(err) console.log(err);
-                res.send();
-              });
+
+        db.all(`select id from collaborators where userid = ${rows[0].id} and listid = ${req.body.listid} and owner = 0`, function(err, rows2) {
+
+          if(rows2 === undefined || rows2.length == 0) {
+            db.run('insert into collaborators (userid, listid, owner) values (?, ?, 0)',
+                  [rows[0].id, req.body.listid],
+                  function(err){
+                    if(err) console.log(err);
+                    res.send();
+                  });
+          } else {
+            res.send();
+          }
+        });
+
       } else {
         res.status(404);
         res.send({error: "User not found."});
